8be799527fd021d6a97efd46991444e69ece3374
Matthew Secrist <matthew.secrist@outlook.com>
2019-02-26 11:17:50 -0600

    Styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
-import { Container } from 'rbx'
+import { Container, Hero, Title } from 'rbx'
 import SearchInput from './components/SearchInput'
 import RepoSelect from './components/RepoSelect'
 import RepoDetails from './components/RepoDetails'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
 const App = () => {
   return (
     <React.Fragment>
+      <Hero>
+        <Hero.Body>
+          <Container textAlign='centered'>
+            <Title>
+              <FontAwesomeIcon icon={faGithub} /> Github Issues Viewer
+            </Title>
+          </Container>
+        </Hero.Body>
+      </Hero>
       <SearchInput />
       <Container>
         <RepoSelect />
